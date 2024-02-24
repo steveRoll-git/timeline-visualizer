45a567f7994cf348ce9730c5f02ffc5285a16ee7
Ron Tseytlin <ronts390@gmail.com>
2024-02-25 00:26:52 +0200

    generate a cell for each day

---
+++ DayCell.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimelineVisualizer
+{
+    /// <summary>
+    /// Interaction logic for DayCell.xaml
+    /// </summary>
+    public partial class DayCell : UserControl
+    {
+        private DateTime date;
+        public DayCell(DateTime date)
+        {
+            InitializeComponent();
+
+            this.date = date;
+            if (date.Day == 1)
+            {
+                DayLabel.Content = $"{date.Day}/{date.Month}";
+            }
+            else
+            {
+                DayLabel.Content = date.Day;
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,15 +9,29 @@ namespace TimelineVisualizer
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// The date that appears in the CalendarGrid's first (top-left) cell.
+        /// </summary>
+        public DateTime TopLeftDate;
+
+        private List<DayCell> dayCells = new();
+
+        private static DateTime FirstDayOfWeek(DateTime date)
+        {
+            var firstDayOfMonth = new DateTime(date.Year, date.Month, 1);
+            return firstDayOfMonth.AddDays(-((7 + (firstDayOfMonth.DayOfWeek - CultureInfo.CurrentCulture.DateTimeFormat.FirstDayOfWeek)) % 7));
+        }
 
         public MainWindow()
         {
+            var dateTimeFormat = CultureInfo.CurrentCulture.DateTimeFormat;
+
             InitializeComponent();
 
             DatePicker.SelectedDate = DateTime.Now;
 
-            var weekdays = CultureInfo.CurrentCulture.DateTimeFormat.AbbreviatedDayNames;
-            for(int i = 0; i < weekdays.Length; i++)
+            var weekdays = dateTimeFormat.AbbreviatedDayNames;
+            for (int i = 0; i < weekdays.Length; i++)
             {
                 var label = new Label()
                 {
@@ -27,6 +41,40 @@ namespace TimelineVisualizer
                 CalendarGrid.Children.Add(label);
                 label.SetValue(Grid.ColumnProperty, i);
             }
+
+            // Initially, the TopLeftDate will be set so that the entire current month will be visible.
+            TopLeftDate = FirstDayOfWeek(DateTime.Today);
+
+            GenerateDayCells();
+        }
+
+        private void GenerateDayCells()
+        {
+            foreach (var cell in dayCells)
+            {
+                CalendarGrid.Children.Remove(cell);
+            }
+            for (int week = 0; week < 6; week++)
+            {
+                for (int weekday = 0; weekday < 7; weekday++)
+                {
+                    var cell = new DayCell(TopLeftDate.AddDays(week * 7 + weekday));
+                    CalendarGrid.Children.Add(cell);
+                    cell.SetValue(Grid.RowProperty, week + 1);
+                    cell.SetValue(Grid.ColumnProperty, weekday);
+                    dayCells.Add(cell);
+                }
+            }
+        }
+
+        private void SetTopLeftDate(DateTime date)
+        {
+            if (TopLeftDate == date)
+            {
+                return;
+            }
+            TopLeftDate = date;
+            GenerateDayCells();
         }
 
         private void LoadJSONMenuItem_Click(object sender, RoutedEventArgs e)
@@ -48,6 +96,24 @@ namespace TimelineVisualizer
                 };
                 loadWindow.StartLoading();
                 loadWindow.ShowDialog();
+            }
+        }
+
+        private void PrevWeekButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetTopLeftDate(TopLeftDate.AddDays(-7));
+        }
+
+        private void NextWeekButton_Click(object sender, RoutedEventArgs e)
+        {
+            SetTopLeftDate(TopLeftDate.AddDays(7));
+        }
+
+        private void DatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (DatePicker.SelectedDate is DateTime date)
+            {
+                SetTopLeftDate(FirstDayOfWeek(new DateTime(date.Year, date.Month, 1)));
             }
         }
     }
